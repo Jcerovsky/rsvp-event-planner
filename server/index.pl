0c77bf19727f81760cc317b675956fd6e758fcee
Jcerovsky <89118515+Jcerovsky@users.noreply.github.com>
2023-11-30 14:03:54 +1030

    connect to database from backend

--- server/index.pl
+++ server/index.pl
@@ -2,3 +2,18 @@
 
 use strict;
 use warnings;
+use JSON;
+use DBI;
+use CGI;
+
+my $query = CGI->new();
+my $method = $query->request_method();
+
+my $data_source_name = "DBI:MariaDB:database=event_planner;host=localhost";
+my $database_handle;
+eval {
+    $database_handle = DBI->connect($data_source_name, 'root', 'Ciro1115!', {RaiseError => 1, AutoCommit => 1});
+};
+if ($@) {
+    print encode_json({error => "Could not connect to database: $@" })
+}
