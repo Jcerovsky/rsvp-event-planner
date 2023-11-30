b12fbfa5b87f96abf5233117cfa5041ffeb576f1
Jcerovsky <89118515+Jcerovsky@users.noreply.github.com>
2023-11-30 17:52:34 +1030

    fix styling for reformatted form

--- client/index.js
+++ client/index.js
@@ -1,7 +1,16 @@
-const rootDiv = document.getElementById("root");
+const rootDivEl = document.getElementById("root");
+const attendingEl = document.getElementById("attending");
 
 const fetchGuests = async () => {
   const response = await fetch("http://localhost/cgi-bin/index.pl");
   const data = await response.json();
   console.log(data);
 };
+
+const handleChangeAttending = () => {
+  if (attendingEl.value === "Yes") {
+    document;
+  }
+};
+
+document.addEventListener("change");
