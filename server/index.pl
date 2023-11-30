ab7b1d4459ebc15225a89d4b9133ecc98e1c1eba
Jcerovsky <89118515+Jcerovsky@users.noreply.github.com>
2023-12-01 06:44:25 +1030

    bug fixes for POST method

--- server/index.pl
+++ server/index.pl
@@ -34,10 +34,17 @@ if ($method eq "POST") {
     my $data = decode_json($query->param("POSTDATA"));
     my $first_name = $data->{'first_name'} ;
     my $last_name = $data->{'last_name'} ;
-    my $attending = $data->{'attending'} ;
+    my $attending = $data->{'attending'}? 1: 0 ;
     my $number_of_attendees = $data->{'number_of_attendees'} ;
     my $dietary_requirements = $data->{'dietary_requirements'} ;
     my $special_requests = $data->{'special_requests'};
-    my $sth = $database_handle->prepare("INSERT INTO rsvp_guests (first_name=?, )")
+    my $sql = "INSERT INTO rsvp_guests (first_name, last_name, attending, number_of_attendees, dietary_requirements, special_requests) VALUES (?,?,?,?,?,?)";
+    my $sth = $database_handle->prepare($sql);
+    if ($sth->execute($first_name, $last_name, $attending, $number_of_attendees, $dietary_requirements, $special_requests)) {
+        print(encode_json({success => 1, message => "Successfully saved data", status => 200}))
+    } else {
+        print(encode_json({error => "Could not save data: $DBI::errstr"}))
+    }
+
 }
 
