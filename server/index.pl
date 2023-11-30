9680a9343d3187c9773efc774dc9304c08bbd106
Jcerovsky <89118515+Jcerovsky@users.noreply.github.com>
2023-11-30 14:11:59 +1030

    create dummy data and add GET method to perl

--- server/index.pl
+++ server/index.pl
@@ -8,7 +8,6 @@ use CGI;
 
 my $query = CGI->new();
 my $method = $query->request_method();
-
 my $data_source_name = "DBI:MariaDB:database=event_planner;host=localhost";
 my $database_handle;
 eval {
@@ -17,3 +16,14 @@ eval {
 if ($@) {
     print encode_json({error => "Could not connect to database: $@" })
 }
+
+if ($method eq 'GET') {
+   my $sth = $database_handle->prepare("SELECT * FROM rsvp_guests");
+    $sth->execute();
+    my @employees;
+    while (my $row = $sth->fetchrow_hashref()) {
+        push(@employees, $row)
+    };
+    print encode_json(@employees)
+}
+
